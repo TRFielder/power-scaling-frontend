9e6895dfc1d0b10943361b2631e5a21bc2551bc5
Tom Fielder <tom.fielder97@gmail.com>
2024-12-29 19:04:31 +0000

    Add base url property to vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,12 @@ import { TanStackRouterVite } from "@tanstack/router-plugin/vite"
 import tailwindcss from "tailwindcss"
 import path from "node:path"
 
+const baseUrl = process.env.VITE_BASE_URL
+
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [TanStackRouterVite({}), react()],
+    base: baseUrl,
     css: {
         postcss: {
             plugins: [tailwindcss()],
