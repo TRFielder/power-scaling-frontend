1d8e737089b41bbed6da057fd20bef7e5545d927
Tom Fielder <tom.fielder97@gmail.com>
2024-12-29 11:48:02 +0000

    Additional styling for input form

--- src/lib/components/CreateCharacterForm/CreateCharacterForm.tsx
+++ src/lib/components/CreateCharacterForm/CreateCharacterForm.tsx
@@ -19,17 +19,21 @@ const CreateCharacterForm = ({
     return (
         <form
             onSubmit={handleSubmit(submitForm)}
-            className="flex flex-col gap-8"
+            className="flex flex-col gap-8 mt-8"
         >
-            <input
-                placeholder="Name"
-                {...register("name", { required: true })}
-            />
-            {errors.name && (
-                <span className="text-red-500 text-sm">
-                    {errors.name.message}
-                </span>
-            )}
+            <section className="flex flex-col">
+                <input
+                    placeholder="Name"
+                    {...register("name", {
+                        required: "A name is required",
+                    })}
+                />
+                {errors.name && (
+                    <span className="text-red-500 text-sm">
+                        {errors.name.message}
+                    </span>
+                )}
+            </section>
             <section className="flex flex-col">
                 <input
                     id="file"
@@ -38,7 +42,8 @@ const CreateCharacterForm = ({
                     {...register("image", {
                         required: true,
                         validate: (fileList) => {
-                            if (!fileList) return "An image file is required"
+                            if (!fileList || !fileList.length)
+                                return "An image file is required"
                             const file = fileList[0]
                             const isImage = file.type.startsWith("image/")
 
