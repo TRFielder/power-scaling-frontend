c136c4d6550d3009e37eed6ad99f2fcf50772f8e
Tom Fielder <tom.fielder97@gmail.com>
2024-12-29 12:03:19 +0000

    Implement battle flow

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,4 +1,8 @@
-import { useGetPairQuery } from "@/lib/api/hooks"
+import {
+    useGetPairQuery,
+    useScoreUpMutation,
+    useScoreDownMutation,
+} from "@/lib/api/hooks"
 import { createFileRoute } from "@tanstack/react-router"
 import CharacterCard from "@/lib/components/CharacterCard/CharacterCard"
 
@@ -7,7 +11,22 @@ export const Route = createFileRoute("/")({
 })
 
 function HomeComponent() {
-    const { data: characterPair, isError, isLoading } = useGetPairQuery()
+    const {
+        data: characterPair,
+        isError,
+        isLoading,
+        refetch,
+    } = useGetPairQuery()
+    const {
+        mutate: incrementScore,
+        isPending: scoreUpPending,
+        isSuccess: scoreUpSuccess,
+    } = useScoreUpMutation()
+    const {
+        mutate: decrementScore,
+        isPending: scoreDownPending,
+        isSuccess: scoreDownSuccess,
+    } = useScoreDownMutation()
 
     if (isLoading)
         return (
@@ -24,21 +43,32 @@ function HomeComponent() {
         )
     }
 
+    const selectWinner = (id: number) => {
+        // Select this id as the winner, mark the other one as the loser
+        const otherId = characterPair.filter(
+            (character) => character.id !== id
+        )[0].id
+
+        incrementScore(id)
+        decrementScore(otherId)
+
+        // Get a new pair and go again
+        if (scoreDownSuccess && scoreUpSuccess) {
+            refetch()
+        }
+    }
+
     return (
         <div className="p-2 flex flex-row gap-6">
             <CharacterCard
                 character={characterPair[0]}
-                disabled={false}
-                selectAsWinner={() =>
-                    window.alert(`You selected ${characterPair[0].name}`)
-                }
+                disabled={scoreUpPending || scoreDownPending}
+                selectAsWinner={selectWinner}
             />
             <CharacterCard
                 character={characterPair[1]}
-                disabled={false}
-                selectAsWinner={() =>
-                    window.alert(`You selected ${characterPair[1].name}`)
-                }
+                disabled={scoreUpPending || scoreDownPending}
+                selectAsWinner={selectWinner}
             />
         </div>
     )
